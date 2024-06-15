8f2b4f382966739d23e8d52d6221bec2295c68d5
sibyabin <sibyabin@users.noreply.github.com>
2024-06-15 21:46:43 +0000

    ✅ Ready to clone and code.

---
+++ sample_app/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for sample_app."""
+
+from sample_app.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from sample_app.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "sample_app"
